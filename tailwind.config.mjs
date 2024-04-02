b5907bc511362e6af9999b258d5967e97ab95852
Dominik Chomicki <chomicki.dominik@gmail.com>
2024-04-02 07:40:55 +0200

    Configure Tailwind theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['"JetBrains Mono Variable"', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        serif: ['Roboto Slab', ...defaultTheme.fontFamily.serif],
+      },
+      screens: {
+        '3xl': '1920px',
+      },
+      colors: {
+        primary: '#8777AD',
+      },
+    },
+  },
+};
