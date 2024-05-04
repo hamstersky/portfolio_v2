0d2b50f85fd04c362140663237ec7744c93560f8
Dominik Chomicki <chomicki.dominik@gmail.com>
2024-05-04 13:21:43 +0200

    feat: add project related content collections

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { z, defineCollection, reference } from 'astro:content';
+
+const projectExcerpts = defineCollection({
+  type: 'data',
+});
+
+const projects = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string(),
+    excerpt: reference('projectExcerpts'),
+  }),
+});
+
+export const collections = {
+  projectExcerpts,
+  projects,
+};
